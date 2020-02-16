6057541904503b72546947cb4f0ab235425c645f
Ashvin-Ranjan <49772172+Ashvin-Ranjan@users.noreply.github.com>
2020-02-15 17:46:14 -0800

    Add files via upload

---
+++ enemyScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyScript : MonoBehaviour
+{
+    public GameObject player;
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(player.transform);
+        transform.Translate(Time.deltaTime * speed * Vector3.forward);
+    }
+}
